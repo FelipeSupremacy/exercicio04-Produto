cd70166bf380a0ec3b3abda9a39ab19ce3800fe0
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-10 11:48:43 -0300

    classe Main e atualização da classe BilheteUnico

--- src/exercicio03/BilheteUnico.java
+++ src/exercicio03/BilheteUnico.java
@@ -3,7 +3,7 @@ package exercicio03;
 import java.util.Random;
 
 public class BilheteUnico {
-    int numeroBilhete;
+    int numero;
     String usuario;
     double saldo;
     String tipoTarifa;
@@ -14,35 +14,30 @@ public class BilheteUnico {
         this.usuario = usuario;
         this.tipoTarifa = tipoTarifa;
         this.saldo = 0;
-        this.numeroBilhete = random.nextInt(1000, 9999);
+        this.numero = random.nextInt(1000, 9999);
     }
 
     public void carregar(double valor) {
-      if (valor <0) {
-          return;
-      }
-      saldo += valor;
+        if(valor <= 0) {
+            return;
+        }
+        saldo += valor;
     }
 
-    public void passarNaCatraca() {
+    public boolean passarNaCatraca() {
         double valor = tarifaBase;
-        if (tipoTarifa.equalsIgnoreCase("estudante") ||
+        if(tipoTarifa.equalsIgnoreCase("estudante") ||
                 tipoTarifa.equalsIgnoreCase("professor")) {
             valor = valor / 2;
         }
 
         // verifica se tem saldo disponível
-        if (saldo >= valor) {
-            return;
+        if(saldo < valor) {
+            return false;
         }
+
         saldo = saldo - valor;
-        }
+        return true;
     }
 
-    public void mostrarDados() {
-        System.out.println("Número do Bilhete: " + numeroBilhete);
-        System.out.println("Usuário: " + usuario);
-        System.out.println("Saldo: " + saldo);
-        System.out.println("Tipo de tarifa: " + tipoTarifa);
-    }
 }
--- src/exercicio03/Main.java
+++ src/exercicio03/Main.java
@@ -1,4 +1,58 @@
 package exercicio03;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner sc = new Scanner(System.in);
+    static BilheteUnico bilhete;
+
+    static {
+        System.out.print("Nome do Usuário --> ");
+        String usuario = sc.nextLine();
+        System.out.print("Tipo de Tarifa (Estudante, Professor ou Normal) --> ");
+        String TipoTarifa = sc.next();
+        bilhete = new BilheteUnico(usuario, TipoTarifa);
+    }
+
+    public static void main(String[] args) {
+        int opcao;
+
+       do {
+            System.out.println("***********************************************");
+            System.out.println("1. Carregar bilhete");
+            System.out.println("2. Consultar Saldo");
+            System.out.println("3. Passar na catraca");
+            System.out.println("4. Finalizar");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1 -> carregar();
+                case 2 -> consultarSaldo();
+                case 3 -> catraca();
+                case 4 -> System.out.println("Obrigado por usar o nosso App");
+                default -> System.out.println("Opcao inválida");
+            }
+
+        }  while (opcao != 4);
+
+    }
+
+    private static void catraca() {
+        if(bilhete.passarNaCatraca() == false) {
+            System.out.println("Saldo insuficiente");
+        }
+        consultarSaldo();
+    }
+
+    private static void consultarSaldo() {
+        System.out.println("Saldo atual R$ " + bilhete.saldo);
+    }
+
+    private static void carregar() {
+        double valor;
+        System.out.println("Valor da recarga --> R$ ");
+        valor = sc.nextDouble();
+        bilhete.carregar(valor);
+
+    }
 }
